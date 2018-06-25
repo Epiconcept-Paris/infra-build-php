b2583fba6d5644fe1de0ef4dc045f21bdf7872b7
Cédric Girard <c.girard@epiconcept.fr>
2018-06-25 17:23:03 +0200

    suppression message de deprecation pour l'extension mysql

--- php/7/mysql.sh
+++ php/7/mysql.sh
@@ -2,6 +2,7 @@
 #	mysql.sh - Install MySQL shared extension
 #
 tar xf $Bld/files/mysql.tgz -C ext
+sed -e '743,745d' -i ext/pecl-database-mysql-master/php_mysql.c
 mv ext/pecl-database-mysql-master ext/mysql
 ExtShow="${ExtShow}MySQL(shared)"
 ExtOpts="$ExtOpts--with-mysql=shared"
