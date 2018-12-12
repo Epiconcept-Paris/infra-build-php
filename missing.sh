42af7a00c5eddadda88f39c1f5195cc1595fb567
Christophe de Traversay <epiconcept@traversay.com>
2018-12-12 11:09:32 +0100

    Add 'latest' command to bake script (display only latest PHP versions) Simplify missing.sh accordingly Add (after check-builds) BUILD_NUM for PHP 5.6.39, 7.0.33 and 7.2.13

--- missing.sh
+++ missing.sh
@@ -1,1 +1,4 @@
-for ver in $(./bake ver | grep -E '[0-9]+[.][0-9]+[.][0-9]+' | sed 's/ //g'); do if [ -z "$(find debian -name "*$ver*")" ]; then echo $ver; fi; done
+for ver in $(./bake latest)
+do
+    test "$(find debian/*/dist -name "*$ver*.deb")" || echo $ver
+done
