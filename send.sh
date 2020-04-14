3c00343f27fb4fb2da5fcef1695c85c480fa3d04
Cédric Girard <cedric@daneel.net>
2020-04-14 23:25:04 +0200

    correction script d'envoi vers files.ec.fr

--- send.sh
+++ send.sh
@@ -2,5 +2,7 @@ tmp=/tmp/sendphp
 rm -fr $tmp
 mkdir $tmp
 cp $(find -name 'epi*.deb') $tmp/
-rsync -rav $tmp/ files.epiconcept.fr:/space/applisdata/php/
+rsync -rav $tmp/ epiconcept_build@files.epiconcept.fr:/space/applisdata/php/
 rm -r $tmp
+
+ssh -t files.epiconcept.fr sudo -u www-data bash -c 'cd /space/www/apps/apt; ./prep.sh update'
