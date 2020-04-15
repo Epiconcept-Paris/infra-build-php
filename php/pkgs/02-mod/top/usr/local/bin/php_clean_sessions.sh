1d280bacd60e28dfe3260eaa61e6355efa40f86b
Cédric Girard <cedric@daneel.net>
2020-04-15 15:00:30 +0200

    correction sur la cron, conversion secondes en minutes et droits d'exécution sur la cron

--- php/pkgs/02-mod/top/usr/local/bin/php_clean_sessions.sh
+++ php/pkgs/02-mod/top/usr/local/bin/php_clean_sessions.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-gc_maxlifetime=$(php -i |grep session.gc_maxlifetime | awk '{print $5}')
+gc_maxlifetime=$(php -i |grep session.gc_maxlifetime | awk '{print $5 / 60}')
 save_path=/var/lib/php/sessions
 sudo find -O3 "$save_path/" -ignore_readdir_race -depth -mindepth 1 -name 'sess_*' -type f -cmin "+$gc_maxlifetime" -delete
