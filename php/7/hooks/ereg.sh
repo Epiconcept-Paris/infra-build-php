ee24c9cadf47c4df8d869ac195418e4dfd3f8036
Christophe de Traversay <epiconcept@traversay.com>
2020-09-28 11:29:42 +0200

    Add check to not use ereg's legacy code if native extension would exist

--- php/7/hooks/ereg.sh
+++ php/7/hooks/ereg.sh
@@ -1,6 +1,7 @@
 #
 #	ereg.sh - Install ereg legacy extension
 #
+test -d ext/ereg && { test "$Dbg" && echo "Extension ereg is native"; return 0; }
 su -c "tar xf $Bld/files/ereg.tar.gz -C ext" $USER
 mv ext/pecl-text-ereg-master ext/ereg	# Required by the extension's code!
 Show="ereg"
