d07dc520e9652e9bb19f32f5e4fcee6a6492a1b3
Christophe de Traversay <epiconcept@traversay.com>
2020-10-12 17:22:55 +0200

    Correct libreadline version in debian/10 (was a temp debian-repo error?)

--- debian/10/Dockervars.sh
+++ debian/10/Dockervars.sh
@@ -2,9 +2,9 @@
 #	debian/10/Dockervars.sh - Define Debian packages required in Dockerfile for build and tests
 #
 BUILD_REQ="vim apt-rdepends file curl build-essential autoconf pkg-config faketime fakeroot lintian apache2 apache2-dev libmcrypt-dev libmhash-dev libjpeg-dev libxpm-dev libxslt1-dev libbz2-dev libxft-dev libmariadbclient-dev libmariadb-dev-compat libcurl4-openssl-dev libssl-dev libpcre3-dev libsystemd-dev libreadline-dev"
-CLI_DEPS="libjpeg62-turbo, libpng16-16, libxpm4, libmcrypt4, libxslt1.1, libcurl4, libfreetype6, libmariadb3, libxml2, libreadline8, libc6"
+CLI_DEPS="libjpeg62-turbo, libpng16-16, libxpm4, libmcrypt4, libxslt1.1, libcurl4, libfreetype6, libmariadb3, libxml2, libreadline7, libc6"
 
-TESTS_REQ="vim curl apache2 libjpeg62-turbo libpng16-16 libxpm4 libmcrypt4 libxslt1.1 libcurl4 libfreetype6 libmariadb3 libreadline8"
+TESTS_REQ="vim curl apache2 libjpeg62-turbo libpng16-16 libxpm4 libmcrypt4 libxslt1.1 libcurl4 libfreetype6 libmariadb3 libreadline7"
 
 DEV72_="libzip-dev"
 LIB72_="libzip4"
