d5467e84a022edab4f543da77deb91522b5e5d06
Cédric Girard <c.girard@epiconcept.fr>
2020-10-13 17:40:31 +0200

    correction script envoi

--- send.sh
+++ send.sh
@@ -5,4 +5,4 @@ cp $(find -name 'epi*.deb') $tmp/
 rsync -rav $tmp/ epiconcept_build@files.epiconcept.fr:/space/applisdata/php/
 rm -r $tmp
 
-ssh -t files.epiconcept.fr sudo -u www-data bash -c 'cd /space/www/apps/apt; ./prep.sh update'
+ssh -t $USER@files.epiconcept.fr sudo -u www-data bash -c 'cd /space/www/apps/apt; ./prep.sh update'
