b3e42da7c7953c4a26a36b70f3fe44063ed737c0
Christophe de Traversay <epiconcept@traversay.com>
2022-06-26 19:51:25 +0200

    Preserve mtimes of *.deb files (with cp -p) in send.sh

--- send.sh
+++ send.sh
@@ -1,10 +1,15 @@
+#!/bin/sh
+
 tmp=/space/tmp/sendphp
 user=epiconcept_build
 server=files.epiconcept.fr
 rm -fr $tmp
 mkdir $tmp
-cp $(find -name 'epi*.deb' | grep -v '/dist/') $tmp/
-rsync -rav $tmp/ $user@$server:/space/applisdata/php/
+#cp $(find -name 'epi*.deb' | grep -v '/dist/') $tmp/
+for i in debian/*/dist/*/*.deb; do
+	cp -p $i $tmp/
+done
+rsync -rltuv $tmp/ $user@$server:/space/applisdata/php/
 rm -r $tmp
 
 ssh -t $user@$server /usr/local/bin/apt_deploy.sh 
