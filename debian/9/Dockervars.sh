a7a079ba10e43c271bfecbeee8ea22b7d03c3684
Christophe de Traversay <epiconcept@traversay.com>
2022-06-26 19:22:02 +0200

    Add MySQL server package to debian/{8,9,10,11}/Dockervars.sh Update php/run/build to use the database server

--- debian/9/Dockervars.sh
+++ debian/9/Dockervars.sh
@@ -1,7 +1,7 @@
 #
 #	debian/9/Dockervars.sh - Define Debian packages required in Dockerfile for build and tests
 #
-BUILD_REQ="vim apt-rdepends file curl build-essential autoconf pkg-config faketime fakeroot lintian apache2 apache2-dev libmcrypt-dev libmhash-dev libjpeg-dev libxpm-dev libxslt1-dev libbz2-dev libxft-dev default-libmysqlclient-dev libcurl4-openssl-dev libssl-dev libpcre3-dev libsystemd-dev libreadline-dev libssh2-1-dev"
+BUILD_REQ="vim apt-rdepends file curl build-essential autoconf pkg-config faketime fakeroot lintian apache2 apache2-dev libmcrypt-dev libmhash-dev libjpeg-dev libxpm-dev libxslt1-dev libbz2-dev libxft-dev default-libmysqlclient-dev libcurl4-openssl-dev libssl-dev libpcre3-dev libsystemd-dev libreadline-dev libssh2-1-dev default-mysql-server"
 CLI_DEPS="libjpeg62-turbo, libpng16-16, libxpm4, libmcrypt4, libxslt1.1, libcurl3, libfreetype6, libmariadbclient18, libxml2, libreadline7, libc6"
 
 TESTS_REQ="vim curl apache2 libjpeg62-turbo libpng16-16 libxpm4 libmcrypt4 libxslt1.1 libcurl3 libfreetype6 libmariadbclient18 libreadline7"
