bdd6d9c1875b536c68a0cc6ad5969c19f58eafca
Christophe de Traversay <epiconcept@traversay.com>
2022-09-01 10:08:29 +0200

    Add built-in extension php-intl:   add missing ICU packages for Debian 8 (jessie) in debian/8/Dockervars.sh   enable extension in php/run/build (except PHP 5.6 on Debian 10 / 11)   increment BUILD_NUM files (except 5.2.17)   add a replacement script for icu-config when missing (finally not used)   add an extra link and install icu-config in php/run/bin/mkenv

--- debian/8/Dockervars.sh
+++ debian/8/Dockervars.sh
@@ -1,10 +1,10 @@
 #
 #	debian/8/Dockervars.sh - Define Debian packages required in Dockerfile for build and tests
 #
-BUILD_REQ="vim apt-rdepends file curl build-essential autoconf pkg-config faketime fakeroot lintian apache2 apache2-dev libmcrypt-dev libmhash-dev libjpeg-dev libxpm-dev libxslt1-dev libbz2-dev libxft-dev libmysqlclient-dev libcurl4-openssl-dev libssl-dev libpcre3-dev libsystemd-dev libreadline-dev libssh2-1-dev mysql-server"
-CLI_DEPS="libjpeg62-turbo, libpng12-0, libxpm4, libmcrypt4, libxslt1.1, libcurl3, libfreetype6, libmysqlclient18, libxml2, libreadline6, libc6"
+BUILD_REQ="vim apt-rdepends file curl build-essential autoconf pkg-config faketime fakeroot lintian apache2 apache2-dev libmcrypt-dev libmhash-dev libjpeg-dev libxpm-dev libxslt1-dev libbz2-dev libxft-dev libmysqlclient-dev libcurl4-openssl-dev libssl-dev libpcre3-dev libsystemd-dev libreadline-dev libssh2-1-dev libicu-dev mysql-server"
+CLI_DEPS="libjpeg62-turbo, libpng12-0, libxpm4, libmcrypt4, libxslt1.1, libcurl3, libfreetype6, libmysqlclient18, libxml2, libreadline6, libicu52, libc6"
 
-TESTS_REQ="vim curl apache2 libjpeg62-turbo libpng12-0 libxpm4 libmcrypt4 libmhash2 libxslt1.1 libcurl3 libfreetype6 libmysqlclient18 libreadline6"
+TESTS_REQ="vim curl apache2 libjpeg62-turbo libpng12-0 libxpm4 libmcrypt4 libmhash2 libxslt1.1 libcurl3 libfreetype6 libmysqlclient18 libreadline6 libicu52"
 
 DEV72_="libzip-dev"
 LIB72_="libzip2"
