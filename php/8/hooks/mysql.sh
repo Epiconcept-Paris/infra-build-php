75223fe06d3ef7b62e57fafeb09173cdd15a0cb5
Christophe de Traversay <epiconcept@traversay.com>
2023-03-29 11:56:36 +0200

    Add new PHP versions 8.1.17 and 8.2.4, with build updates for them

--- php/8/hooks/mysql.sh
+++ php/8/hooks/mysql.sh
@@ -7,3 +7,9 @@ Patch 0 "$Bld/files/mysql.patch"
 Patch 0 "$Bld/files/mysqlnd.patch"
 Show="MySQL(shared)"
 Opt="--with-mysql=shared"
+PdoMysql=
+MysqlI=
+if VerCmp $Ver -lt '8.1.17'; then
+    PdoMysql='=/usr'
+    MysqlI='=/usr/bin/mysql_config'
+fi
