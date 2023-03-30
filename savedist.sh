0c43484e841427af3e8648ca7196ce4241706385
Christophe de Traversay <epiconcept@traversay.com>
2023-03-30 11:27:42 +0200

    Add checks and 'multi' to savedist.sh

--- savedist.sh
+++ savedist.sh
@@ -2,23 +2,36 @@
 
 Prg="$(basename "$0")"
 DebDir='../php-debs'
+test "$*" && echo "NOTE: arguments '$*' to $Prg will be ignored" >&2
+#   Test below assumes 'php-prod' and 'infra-build-php' are not in the same dir
 test -d "$DebDir" -a -d 'debian' || { echo "$Prg: for php-prod builds only" >&2; exit 1; }
 for dv in 8 9 10 11
 do
-    for d in "debian/$dv/dist"/*
+    for d in "debian/$dv/dist"/* "debian/$dv/multi"
     do
-	test -d "$d" || break
+	test -d "$d" || continue	# Skip if pattern does not exist
 	pv=$(basename $d)
 	if [ -d "$DebDir/$dv/$pv" ]; then
+	    bad=
+	    case "$pv" in
+		[5-9].*)    test -f $d/epi-php-*-cli_*.deb || bad=y;;
+		tools)	    test -f $d/epi-tools-waitpid_*.deb || bad=y;;
+		multi)	    test -d $d/pkgs -a -d $d/logs || bad=y;;
+	    esac
+	    test "$bad" && { echo "Skipping $d (incomplete build)" >&2; continue; }
 	    diff=
 	    for f in $(find $d -type f | sed "s;^$d/;;")
 	    do
 		cmp "$d/$f" "$DebDir/$dv/$pv/$f" >/dev/null 2>&1 || { diff='y'; break; }
 	    done
-	    test "$diff" && echo "$DebDir/$dv/$pv exists - rename it if needed" || echo "Skipping $d (already saved)"
+	    test "$diff" && echo "Dist $d differs from $DebDir/$dv/$pv - rename/delete that if needed" || echo "Dist $d is already saved"
 	else
 	    echo "Saving $d"
-	    tar Ccf "debian/$dv/dist" - "$pv" | tar Cxf "$DebDir/$dv" -
+	    if [ "$pv" = 'multi' ]; then
+		tar Ccf "debian/$dv" - "$pv" | tar Cxf "$DebDir/$dv" -
+	    else
+		tar Ccf "debian/$dv/dist" - "$pv" | tar Cxf "$DebDir/$dv" -
+	    fi
 	fi
     done
 done
