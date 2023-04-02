1adb39c2a6bf71f05142b8112939405b910c77e6
Christophe de Traversay <epiconcept@traversay.com>
2023-04-02 17:04:59 +0200

    Add update of /etc/apt/sources.list (via $APT_SRC) in build of containers Use it only for Debian 8 (jessie) so far

--- debian/8/Dockervars.sh
+++ debian/8/Dockervars.sh
@@ -1,6 +1,7 @@
 #
 #	debian/8/Dockervars.sh - Define Debian packages required in Dockerfile for build and tests
 #
+APT_SRC="RUN echo \"deb [trusted=yes] http://archive.debian.org/debian jessie main\\\\ndeb [trusted=yes] http://archive.debian.org/debian-security/ jessie/updates main\" >/etc/apt/sources.list"
 BUILD_REQ="vim apt-rdepends file curl build-essential autoconf pkg-config faketime fakeroot lintian apache2 apache2-dev libmcrypt-dev libmhash-dev libjpeg-dev libxpm-dev libxslt1-dev libbz2-dev libxft-dev libmysqlclient-dev libcurl4-openssl-dev libssl-dev libpcre3-dev libsystemd-dev libreadline-dev libssh2-1-dev libicu-dev mysql-server"
 CLI_DEPS="libjpeg62-turbo, libpng12-0, libxpm4, libmcrypt4, libxslt1.1, libcurl3, libfreetype6, libmysqlclient18, libxml2, libreadline6, libicu52, libc6"
 
