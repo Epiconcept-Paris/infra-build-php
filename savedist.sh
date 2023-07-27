6d47b774f2437cdd3844cb73aaa12b39e60acd65
Christophe de Traversay <epiconcept@traversay.com>
2023-07-27 09:22:08 +0200

    Enhance, add comments to and shellcheck savedist.sh and send.sh

--- savedist.sh
+++ savedist.sh
@@ -1,26 +1,40 @@
 #!/bin/sh
-
+#
+#	savedist.sh - Save prod versions in '../php-debs' side directory
+#
+# shellcheck disable=SC2164	# Use 'cd ... || exit'
+#
+Usr=$(id -un)
 Prg="$(basename "$0")"
+Dir="$(dirname "$0")"
+cd "$Dir"
+
 DebDir='../php-debs'
+NoDir="$Prg: cannot find directory"
+
 test "$*" && echo "NOTE: arguments '$*' to $Prg will be ignored" >&2
+test "$Usr" = 'php' || { echo "$Prg: for the 'php' user only" >&2; exit 1; }
+test -d 'debian' || { echo "$NoDir 'debian' in '$PWD'" >&2; exit 1; }
+
 #   Test below assumes 'php-prod' and 'infra-build-php' are not in the same dir
-test -d "$DebDir" -a -d 'debian' || { echo "$Prg: for php-prod builds only" >&2; exit 1; }
-for dv in 8 9 10 11
+test -d "$DebDir" || { echo "$NoDir '$(basename "$DebDir")' in '$(dirname "$PWD")'" >&2; exit 1; }
+
+for dv in $(cd 'debian'; echo [0-9] [0-9][0-9])
 do
     for d in "debian/$dv/dist"/* "debian/$dv/multi"
     do
 	test -d "$d" || continue	# Skip if pattern does not exist
-	pv=$(basename $d)
+	pv=$(basename "$d")
 	if [ -d "$DebDir/$dv/$pv" ]; then
 	    bad=
 	    case "$pv" in
-		[5-9].*)    test -f $d/epi-php-*-cli_*.deb || bad=y;;
-		tools)	    test -f $d/epi-tools-waitpid_*.deb || bad=y;;
-		multi)	    test -d $d/pkgs -a -d $d/logs || bad=y;;
+		[5-9].*)    test -f "$d"/epi-php-*-cli_*.deb || bad=y;;
+		tools)	    test -f "$d"/epi-tools-waitpid_*.deb || bad=y;;
+		multi)	    test -d "$d"/pkgs -a -d "$d"/logs || bad=y;;
 	    esac
 	    test "$bad" && { echo "Skipping $d (incomplete build)" >&2; continue; }
 	    diff=
-	    for f in $(find $d -type f | sed "s;^$d/;;")
+	    for f in $(find "$d" -type f | sed "s;^$d/;;")
 	    do
 		cmp "$d/$f" "$DebDir/$dv/$pv/$f" >/dev/null 2>&1 || { diff='y'; break; }
 	    done
--- send.sh
+++ send.sh
@@ -1,16 +1,41 @@
 #!/bin/sh
+#
+#	send.sh - Push packages to files.epiconcept.fr
+#
+# shellcheck disable=SC2164	# Use 'cd ... || exit'
+#
+Prg=$(basename "$0")
+Dir="$(dirname "$0")"
+cd "$Dir"
 
 dist=../php-debs
 tmp=/space/tmp/sendphp
-user=epiconcept_build
-server=files.epiconcept.fr
-test -d $dist || { echo "$0: cannot find '$dist'" >&2; exit 1; }
+usr=epiconcept_build
+srv=files.epiconcept.fr
+
+test -d $dist || { echo "$Prg: cannot find '$(realpath $dist)'" >&2; exit 1; }
+
+cleanup()
+{
+    echo "Deleting $tmp/"
+    rm -rf $tmp
+}
+
 rm -fr $tmp
 mkdir $tmp
-for i in $dist/*/*/*.deb; do
-	cp -p $i $tmp/
+for deb in "$dist"/*/*/*.deb; do
+    cp -p "$deb" $tmp/
 done
-rsync -rltuv $tmp/ $user@$server:/space/applisdata/php/
-rm -r $tmp
+trap cleanup 0
 
-ssh -t $user@$server /usr/local/bin/apt_deploy.sh 
+rsync -rltuv $tmp/ $usr@$srv:/space/applisdata/php/ || {
+    xc=$?
+    echo "$Prg: rsync failed (xc=$xc)" >&2
+    exit $xc
+}
+
+ssh -t $usr@$srv /usr/local/bin/apt_deploy.sh || {
+    xc=$?
+    echo "$Prg: apt_deploy.sh on '$srv' failed (xc=$xc)" >&2;
+    exit $xc
+}
