29f09ddd45502e7900048123c54c1733cc3dbbdd
Christophe de Traversay <epiconcept@traversay.com>
2023-08-04 19:14:05 +0200

    Enhance update.sh:   - remove old files in update.log/   - remove old docker images   - report number of save diffs   - correct most shellcheck messages

--- update.sh
+++ update.sh
@@ -33,6 +33,8 @@
 #
 # shellcheck disable=SC2086	# Double quote to prevent globbing
 # shellcheck disable=SC2039	# In POSIX sh, 'local' is undefined
+# shellcheck disable=SC2059	# Don't use variables in the printf format string
+# shellcheck disable=SC2164	# Use 'cd ... || exit'
 #
 Prg=$(basename "$0")
 Dir=$(dirname "$0")
@@ -46,6 +48,117 @@ odate()
     now=$(date '+%s')
     old=$((now - $1))
     date -d "@$old" '+%Y-%m-%d %H:%M:%S'
+}
+
+#   Return deb-name given deb-num
+debname()
+{
+    local n
+
+    case $1 in
+	 8) n='jessie';;
+	 9) n='stretch';;
+	10) n='buster';;
+	11) n='bullseye';;
+	12) n='bookworm';;
+	13) n='trixie';;
+    esac
+    echo $n
+}
+
+#   Return info on existing docker images for all releases of PHP major.minor $1.$2
+pmm_dis()
+{
+    #global DkIms
+
+    # Sample result of docker images
+    #	REPOSITORY      TAG               IMAGE ID       CREATED         SIZE
+    #	dev-build-php   stretch-8.1.21    bdd805140711   4 days ago      973MB
+    #	dev-tests-php   buster-8.2.5      2e86b4147f78   5 days ago      308MB
+    #	dev-build-php   bullseye-8.2.8    f419049cc46e   26 hours ago    1.12GB
+    #	dev-build-php   buster-8.2.8      2c96c256dbf2   26 hours ago    963MB
+    #
+    # Given for example $1 = 8 and $2 = 2 (PHP 8.2), we want
+    #
+    #	8-10-build 8-11-build 5-10-tests
+    #
+    echo "$DkIms" | awk 'BEGIN {
+	# Init array of Debian version names to numbers
+	dn["jessie"]   =  8
+	dn["stretch"]  =  9
+	dn["buster"]   = 10
+	dn["bullseye"] = 11
+	dn["bookworm"] = 12
+	dn["trixie"]   = 13
+	s = " "
+    }
+    #	For all existing dev-*-php images
+    {
+	# split column 2
+	split($2, d, "[-.]")
+	print d[2] s d[3] s d[4] s dn[d[1]] s gensub(/^dev-([^\-]+)-php/, "\\1", 1, $1)
+    }' |
+	# We sort in descending order of releases (field 3)
+	sort -k1n,1 -k2n,2 -k3nr,3 -k4n,4 -k5,5 |
+	# We only output columns 3,4,5 as 1 & 2 are known
+	awk "\$1==\"$1\" && \$2==\"$2\" {printf(\"%s%d-%d-%s\",sep,\$3,\$4,\$5); sep=\" \"}"
+}
+
+#   Remove old docker images
+#	$@ is a list of (new) PHP version(s)
+rm_odis()
+{
+    #global MaxRodi
+    local DkIms pv mj mn rl cr nr ni ii rel deb tag rm nd img out
+
+    #	We keep only dev-*-php repos
+    #	Prod's epi-*-php repos are handled by savedist.sh
+    DkIms="$(docker images | grep 'dev-[^- ]*-php')"
+
+    #   For all new version(s) mn.mn.rl
+    for pv in "$@"
+    do
+	eval "$(echo $pv | sed -r 's/^([0-9]+)\.([0-9]+)\.([0-9]+)$/mj=\1 mn=\2 rl=\3/')"
+	#echo "mj=$mj mn=$mn rl=$rl" >&2	# DBG
+
+	cr=	# current rel
+	nr=0	# number of rel since top
+	ni=0	# number of images
+	rm=
+	#   For each image info (rel-deb-tag)
+	for ii in $(pmm_dis $mj $mn)
+	do
+	    eval "$(echo $ii | sed -r 's/^([0-9]+)-([0-9]+)-([a-z]+)$/rel=\1 deb=\2 tag=\3/')"
+	    if [ "$rel" != "$cr" ]; then
+		#echo -n "rel=$rel" >&2							# DBG
+		#test "$cr" && echo " (was rel=$cr ni=$ni nr=$nr)" >&2 || echo >&2	# DBG
+		cr="$rel"	# rel changed
+		nr=$((nr + 1))
+		ni=1
+	    else
+		ni=$((ni + 1))
+	    fi
+	    #echo "cr=$cr deb=$deb tag=$tag ni=$ni" >&2	# DBG
+	    test "$nr" -le "$MaxRodi" && continue
+	    test "$rm" && rm="$rm "
+	    rm="${rm}dev-$tag-php:$(debname $deb)-$mj.$mn.$rel"
+	done
+	if [ "$rm" ]; then
+	    nd=0
+	    for img in $rm
+	    do
+		if out="$(docker rmi $img 2>&1)"; then
+		    nd=$((nd + 1))
+		    echo "Removed '$img' docker image" >&2
+		else
+		    echo "ERROR on 'docker rmi $img':\n$out" >&2
+		fi
+	    done
+	    echo "Removed $nd/$ni dev-*-php docker images for PHP $mj.$mn.$cr"
+	#else						# DBG
+	#    echo "last cr=$cr ni=$ni nr=$nr" >&2	# DBG
+	fi
+    done
 }
 
 #   Build $1 (dev/prod) PHP version $2 for Debian $3 (1st and 2nd instances)
@@ -66,7 +179,7 @@ build()
     if [ $xc -eq 0 ]; then
 	mis=$(bin/chkdebs $3 $2)
 	test "$mis" && {
-	    echo "WARNING: missing $mis packages for PHP $1 on Debian $2$CR"; 
+	    echo "WARNING: missing $mis packages for PHP $1 on Debian $2$CR";
 	    echo "$xc $mis" >$ff
 	}
     else
@@ -84,7 +197,7 @@ failed()
     for bf in debian/*/dist/*/.fail
     do
 	test -f $bf || continue
-	eval $(echo $bf | sed -r 's;debian/([0-9]+)/dist/([.0-9]+)-([0-9]+)/\.fail;dv=\1 pv=\2 bn=\3;')
+	eval "$(echo $bf | sed -r 's;debian/([0-9]+)/dist/([.0-9]+)-([0-9]+)/\.fail;dv=\1 pv=\2 bn=\3;')"
 	test "$bn" -ne 1 && { echo "WARNING: '$bf' on BUILD_NUM '$bn' > 1. Discarded.$CR"; continue; }
 	test "$fail" && fail="$fail "
 	fail="${fail}fail:$pv:$dv"
@@ -101,7 +214,7 @@ bldnum()
 #   Phase 1 - Build 'dev' packages (ran as (non-'php') user)
 phase1()
 {
-    local debs phps blds res fail prod xc dv pv bf bn dd
+    local debs phps blds res fail prod xc dv pv bf bn dd s
 
     test "$#" -gt 0 && { echo "$Prg: no arguments needed" >&3; exit 1; }
 
@@ -116,6 +229,7 @@ phase1()
 
     #   Determine possible new PHP versions
     #	We check the existence of BUILD_NUM files in $(git ls-files)
+    #	to determine if a version is already known
     phps=
     blds=
     res=
@@ -152,7 +266,8 @@ phase1()
 	echo '1' >"$bf"
 	git add "$bf"
     done
-    git commit -m "Add new PHP versions $(echo $res | sed 's/,/, /g')" | sed 's/$//'
+    echo "$res" | grep ',' >/dev/null && s='s' || s=
+    git commit -m "Add new PHP version$s $(echo $res | sed 's/,/, /g')" | sed 's/$//'
 
     #	Bake 'dev' builds
     prod=
@@ -176,13 +291,14 @@ phase1()
     #	sudo below assumes rights to call $PhpDir/$Prg as 'php' without password
     #	Sample sudoers.d entry:
     #	    # Allow 'cdt' to run php-prod/update.sh as 'php' without password
-    #	    cdt ALL = (php) NOPASSWD: php-prod/update.sh
+    #	    cdt ALL = (php) NOPASSWD: /home/php/php-prod/update.sh
     #
     #	There MUST be a symlink $PhpDir to the actual 'prod' git repo
     #
     test "$prod" && res="$res $(sudo -iu 'php' "$PhpDir/$Prg" $prod)"
     echo "Res=\"$res\"$CR"
     report $res >&3
+    rm_odis $phps >&3	# Remove old docker images
 }
 
 #   Phase 2 - Build 'prod' packages (ran as 'php' user)
@@ -198,7 +314,7 @@ phase2()
     #	Build PHP versions
     for arg in "$@"
     do
-	eval $(echo $arg | awk -F: '{printf("pv=%s dv=%s", $1, $2)}')
+	eval "$(echo $arg | awk -F: '{printf("pv=%s dv=%s", $1, $2)}')"
 	#echo "arg=$arg pv=$pv dv=$dv$CR"
 	build 'prod' $pv $dv
 	xc=$?
@@ -267,7 +383,7 @@ report()
 	    fail)
 		ff="$bd/.fail"
 		if [ -f "$ff" ]; then
-		    read xc mis < "$ff"
+		    read -r xc mis < "$ff"
 		    if [ "$xc" -ne 0 ]; then
 			printf "$mB $mF (xc=%s)\n" "$mP 'dev'" $pv $dv $xc
 		    elif [ "$mis" ]; then
@@ -287,7 +403,7 @@ report()
 		if [ "$xc" -eq 0 ]; then
 		    ff="$bd/.fail"
 		    if [ -f "$ff" ]; then
-			read xc mis < "$ff"
+			read -r xc mis < "$ff"
 			printf "$mB $mS, but a '$ff' file\n" "'$bt'" $pv $dv
 			echo "    shows that packages $mis are missing."
 			echo "    $mL"
@@ -302,14 +418,36 @@ report()
 
 	    save|send)
 		test "$bt" = 'save' && echo -n "\n$mD" || echo -n "$mR"
-		if [ "$xc" -ne 0 ]; then
-		    echo " $mF (xc=$xc)"
-		else
+		if [ "$xc" -eq 0 ]; then
 		    echo " $mS."
+		    test "$bt" = 'save' && save_diffs
+		else
+		    echo " $mF (xc=$xc)"
 		fi
 		;;
 	esac
     done
+}
+
+#   Report on save diffs if any
+save_diffs()
+{
+    #global PhpTop
+    local log nbd dir rep nbt
+
+    log="$PhpTop/update.log/$(date '+%Y-%m-%d')"
+    nbd=$(grep -c 'differs from' "$log")
+    test "$nbd" -eq 0 && return
+    dir="$PhpTop"
+    test -L "$dir" && dir="$(readlink "$dir")"
+    rep="$(dirname "$dir")/php-debs"
+    nbt=$(cd "$rep"; find [0-9] [1-9][0-9] -maxdepth 1 -type d | grep / | sort -n | wc -l)
+    if [ "$nbd" -eq 1 ]; then
+	echo "    but $nbd dist / $nbt total differs from its saved version"
+    else
+	echo "    but $nbd dists / $nbt total differ from their saved version(s)"
+    fi
+    echo "    (see $log for details)"
 }
 
 # ===== Main ===================================
@@ -318,6 +456,7 @@ report()
 grep '^php:' /etc/passwd >/dev/null || { echo "$Prg: no 'php' user (required) on $(hostname)" >&2; exit 1; }
 
 #   Setup Globals
+MaxRodi=3	# Max PHP releases (per maj.min) we preserve old docker images for
 PhpHome=~php
 PhpDir='php-prod'
 PhpTop="$PhpHome/$PhpDir"
@@ -325,7 +464,6 @@ Bake='php/bake'
 eval "$(grep 'http_proxy' ~/.bash_profile)"
 Usr=$(id -un)
 CR=''
-xmp=42	# exit-code for missing packages
 
 #   Setup Log and fds
 LogDir="$(basename $Prg .sh).log"
@@ -333,6 +471,7 @@ Log="$LogDir/$(date '+%Y-%m-%d')"
 Fifo="$LogDir/.fifo"
 mkdir -p $LogDir
 test -p "$Fifo" || mkfifo "$Fifo"
+find $LogDir -type f -mtime +90 | xargs rm -f
 #   All script stdout and stderr will go to $Log
 #   To put message in the final email, output to fd:3
 exec 3>&1	# Save stdout
