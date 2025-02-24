1c474407c626670954cd751542b1bd918f0f8dd7
Christophe de Traversay <epiconcept@traversay.com>
2025-02-24 15:07:58 +0100

    Add the php-imap extension (in a separate package) to the standard builds

--- debian/11/Dockervars.sh
+++ debian/11/Dockervars.sh
@@ -2,10 +2,10 @@
 #	debian/11/Dockervars.sh - Define Debian packages required in Dockerfile for build and tests
 #
 APT_SRC=
-BUILD_REQ="vim apt-rdepends file curl build-essential autoconf pkg-config faketime fakeroot lintian apache2 apache2-dev libmcrypt-dev libmhash-dev libjpeg-dev libxpm-dev libxslt1-dev libbz2-dev libxft-dev libmariadbd-dev libmariadb-dev libmariadb-dev-compat libcurl4-openssl-dev libssl-dev libpcre3-dev libsystemd-dev libreadline-dev libssh2-1-dev default-mysql-server"
+BUILD_REQ="vim apt-rdepends file curl build-essential autoconf pkg-config faketime fakeroot lintian apache2 apache2-dev libmcrypt-dev libmhash-dev libjpeg-dev libxpm-dev libxslt1-dev libbz2-dev libxft-dev libmariadbd-dev libmariadb-dev libmariadb-dev-compat libcurl4-openssl-dev libssl-dev libpcre3-dev libsystemd-dev libreadline-dev libssh2-1-dev libc-client2007e-dev libkrb5-dev default-mysql-server"
 CLI_DEPS="libjpeg62-turbo, libpng16-16, libxpm4, libmcrypt4, libxslt1.1, libcurl4, libfreetype6, libmariadb3, libxml2, libreadline8, libc6"
 
-TESTS_REQ="vim curl apache2 libjpeg62-turbo libpng16-16 libxpm4 libmcrypt4 libxslt1.1 libcurl4 libfreetype6 libmariadb3 libreadline8"
+TESTS_REQ="vim curl apache2 libjpeg62-turbo libpng16-16 libxpm4 libmcrypt4 libxslt1.1 libcurl4 libfreetype6 libmariadb3 libreadline8 libc-client2007e"
 
 DEV72_="libzip-dev"
 LIB72_="libzip4"
