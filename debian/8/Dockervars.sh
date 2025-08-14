01cb030e1cb5f76fa313d8829c773ef22c5c139c
Christophe de Traversay <epiconcept@traversay.com>
2025-08-14 16:08:06 +0200

    Harmonize APT_SRC in debian/[89]/Dockervars.sh

--- debian/8/Dockervars.sh
+++ debian/8/Dockervars.sh
@@ -1,7 +1,7 @@
 #
 #	debian/8/Dockervars.sh - Define Debian packages required in Dockerfile for build and tests
 #
-APT_SRC="RUN sed -ri -e 's/^deb /deb [trusted=yes] /' -e 's/(deb|security)\.debian/archive.debian/' -e '/ jessie-updates /d' /etc/apt/sources.list"
+APT_SRC="RUN sed -ri -e 's/(deb|security)\.debian/archive.debian/' -e '/ jessie-updates /d' -e 's/^deb /deb [trusted=yes] /' /etc/apt/sources.list"
 BUILD_REQ="vim apt-rdepends file curl build-essential autoconf pkg-config faketime fakeroot lintian apache2 apache2-dev libmcrypt-dev libmhash-dev libjpeg-dev libxpm-dev libxslt1-dev libbz2-dev libxft-dev libmysqlclient-dev libcurl4-openssl-dev libssl-dev libpcre3-dev libsystemd-dev libreadline-dev libssh2-1-dev libicu-dev libc-client2007e-dev libkrb5-dev mysql-server"
 CLI_DEPS="libjpeg62-turbo, libpng12-0, libxpm4, libmcrypt4, libxslt1.1, libcurl3, libfreetype6, libmysqlclient18, libxml2, libreadline6, libicu52, libc6"
 
--- debian/9/Dockervars.sh
+++ debian/9/Dockervars.sh
@@ -1,7 +1,7 @@
 #
 #	debian/9/Dockervars.sh - Define Debian packages required in Dockerfile for build and tests
 #
-APT_SRC="RUN sed -i -r -e 's/(deb|security)\.debian/archive.debian/' -e 's/stretch-updates/stretch-proposed-updates/' /etc/apt/sources.list"
+APT_SRC="RUN sed -ri -e 's/(deb|security)\.debian/archive.debian/' -e 's/stretch-updates/stretch-proposed-updates/' /etc/apt/sources.list"
 BUILD_REQ="vim apt-rdepends file curl build-essential autoconf pkg-config faketime fakeroot lintian apache2 apache2-dev libmcrypt-dev libmhash-dev libjpeg-dev libxpm-dev libxslt1-dev libbz2-dev libxft-dev default-libmysqlclient-dev libcurl4-openssl-dev libssl-dev libpcre3-dev libsystemd-dev libreadline-dev libssh2-1-dev libc-client2007e-dev libkrb5-dev default-mysql-server"
 CLI_DEPS="libjpeg62-turbo, libpng16-16, libxpm4, libmcrypt4, libxslt1.1, libcurl3, libfreetype6, libmariadbclient18, libxml2, libreadline7, libc6"
 
