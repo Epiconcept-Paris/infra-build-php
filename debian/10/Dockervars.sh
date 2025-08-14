f862eedd5f0e9b117bd8ec6f2b55a34f07fa7ead
Christophe de Traversay <epiconcept@traversay.com>
2025-08-14 15:13:40 +0200

    Update debian/10/Dockervars.sh to use archive.debian.org

--- debian/10/Dockervars.sh
+++ debian/10/Dockervars.sh
@@ -1,7 +1,7 @@
 #
 #	debian/10/Dockervars.sh - Define Debian packages required in Dockerfile for build and tests
 #
-APT_SRC=
+APT_SRC="RUN sed -ri -e 's/(deb|security)\.debian/archive.debian/' -e 's/buster-updates/buster-proposed-updates/' /etc/apt/sources.list"
 BUILD_REQ="vim apt-rdepends file curl build-essential autoconf pkg-config faketime fakeroot lintian apache2 apache2-dev libmcrypt-dev libmhash-dev libjpeg-dev libxpm-dev libxslt1-dev libbz2-dev libxft-dev libmariadbclient-dev libmariadb-dev-compat libcurl4-openssl-dev libssl-dev libpcre3-dev libsystemd-dev libreadline-dev libssh2-1-dev libc-client2007e-dev libkrb5-dev default-mysql-server"
 CLI_DEPS="libjpeg62-turbo, libpng16-16, libxpm4, libmcrypt4, libxslt1.1, libcurl4, libfreetype6, libmariadb3, libxml2, libreadline7, libc6"
 
